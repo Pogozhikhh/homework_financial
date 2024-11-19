282102bbe03394d33032783fb34c276569ba6690
Pogozhikhh <Pogozhikhh@yandex.ru>
2024-11-19 11:11:28 +0300

    add function filter_by_state and sort_by_date

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+def filter_by_state(list_of_dicts: list[dict[str, any]], state: str = "EXECUTED") -> any:
+    """Функция принимает список словарей и значение для ключа и возвращает новый
+    список содержащий только те словари у которых ключ содержит переданное значение"""
+    return [t for t in list_of_dicts if t.get("state") == state]
+
+
+
+def sort_by_date(list_of_dicts: list[dict[str, any]], reverse_list: bool = True) -> list[dict[str, any]]:
+    """Функция принимает список и сортирует его по убыванию"""
+
+    return sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse_list)
