fa6358d99f8029d3e53c50d4b58d8cd08188eff4
Pogozhikhh <Pogozhikhh@yandex.ru>
2024-11-22 16:44:48 +0300

    Change function get_date and write test for her

--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,12 @@
 from typing import Any
 
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(type_card: Any) -> str:
     """Вывод замаскированных счетов и карт"""
     if type_card == "":
-        raise ValueError("нет данных")
+        raise ValueError('нет данных')
     else:
         if "Счет" in type_card:
             return "Счет " + get_mask_account(type_card[-16:])
@@ -18,8 +18,11 @@ def mask_account_card(type_card: Any) -> str:
 
 def get_date(data: Any) -> str:
     """Функция преобразующая дату"""
-
-    return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
+    data_list = data.split('-')
+    if data_list[0].isdigit() and data_list[1].isdigit() and data_list[2][:2].isdigit and len(data_list) == 3:
+        returned_date = data_list[2][:2] + '.' + data_list[1] + '.' + data_list[0]
+        return returned_date
+    raise ValueError('Некорректный формат даты')
 
 
 # print(mask_account_card("Maestro 1596837868705199"))
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -6,6 +6,24 @@ from src.widget import get_date, mask_account_card
 def test_mask_account_card_type():
     pass
 
+@pytest.mark.parametrize('value, expected', [
+    ('2024-03-11T02:26:18.671407', '11.03.2024'),
+    ('2024-08-06', '06.08.2024')
+])
 
-def test_get_date():
-    assert get_date("2024-03-11T02:26:18.671407") == '11.03.2024'
+
+def test_get_date(value, expected):
+    assert get_date(value) == expected
+
+
+
+@pytest.mark.parametrize('value, expected', [
+    ('20/12/2023', 'Некорректный формат даты'),
+    ('06.05.2021', 'Некорректный формат даты'),
+    ('двадцать второе июня сорок первого года', 'Некорректный формат даты'),
+    ('aa-bb-cc', 'Некорректный формат даты'),
+    ('', 'Некорректный формат даты')
+])
+def test_get_date_valueerror(value, expected):
+    with pytest.raises(ValueError):
+        get_date(value) == expected
