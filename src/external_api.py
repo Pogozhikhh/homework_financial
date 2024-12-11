a26eb24ce2f5ebc36df925db5804cf617bc99162
Pogozhikhh <Pogozhikhh@yandex.ru>
2024-12-11 18:11:55 +0300

    created module external_api and fuction convertation and created function sum transactions

--- src/external_api.py
+++ src/external_api.py
@@ -1,2 +1,19 @@
 import requests
+import os
+from dotenv import load_dotenv
+from typing import Any
 
+
+
+load_dotenv()
+values = os.getenv("API-KEY")
+def conversion_currency(transactions: Any) -> Any:
+    """Функция конвертации"""
+    amount = transactions["operationAmount"]["amount"]
+    code = transactions["operationAmount"]["currency"]["code"]
+    to = "RUB"
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amount}"
+    payload = {}
+    response = requests.get(url, headers={"apikey": values}, data=payload)
+    result = response.json()
+    return result["result"]
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+from src.external_api import conversion_currency
 
 
 def prepare_json_file(json_file: str) -> dict | list:
@@ -10,6 +11,14 @@ def prepare_json_file(json_file: str) -> dict | list:
             return transactions
     else:
         return empty_list
+
+def transaction_amount(trans: dict, currency: str = "RUB") -> float:
+    """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
+    if trans["operationAmount"]["currency"]["code"] == currency:
+        amount = trans["operationAmount"]["amount"]
+    else:
+        amount = conversion_currency(trans)
+    return amount
 
 print(prepare_json_file(r'C:\Users\111\PycharmProjects\Poetry_Test\data\operations.json'))
 print(type(prepare_json_file(r'C:\Users\111\PycharmProjects\Poetry_Test\data\operations.json')))
