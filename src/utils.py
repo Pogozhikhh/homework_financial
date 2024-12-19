8f86888a296a74150464536eb7ff284f0c884c89
Pogozhikhh <Pogozhikhh@yandex.ru>
2024-12-19 16:39:23 +0300

    changed function prepare_json_file and created test utils

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,22 @@
 import json
+from json import JSONDecodeError
 
 from src.external_api import conversion_currency
 
 
 def prepare_json_file(json_file: str) -> dict | list:
     """Функция, преобразующая json файл в формат python"""
-    empty_list = []
-    if json_file is not list and len(json_file) > 0:
-        with open(json_file, "r", encoding="utf-8") as f:
-            transactions = json.load(f)
-            return transactions
-    else:
-        return empty_list
+    try:
+        with open(json_file, encoding="utf-8") as financial_file:
+            try:
+                transactions = json.load(financial_file)
+            except JSONDecodeError:
+                return []
+        if not isinstance(transactions, list):
+            return []
+        return transactions
+    except FileNotFoundError:
+        return []
 
 
 def transaction_amount(trans: dict, currency: str = "RUB") -> float:
@@ -25,4 +30,3 @@ def transaction_amount(trans: dict, currency: str = "RUB") -> float:
 
 # print(prepare_json_file(r"C:\Users\111\PycharmProjects\Poetry_Test\data\operations.json"))
 # print(type(prepare_json_file(r"C:\Users\111\PycharmProjects\Poetry_Test\data\operations.json")))
-
